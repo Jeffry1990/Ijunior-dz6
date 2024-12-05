b8f0a855393991885f946ed41ef71b560fae70a8
Jeffry1990 <109137757+Jeffry1990@users.noreply.github.com>
2024-12-05 14:09:53 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.ConstrainedExecution;
+
+namespace ConsoleApp45
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int peoplesInQueue;
+            int minutesPerPerson = 10;
+            int waitingTime;
+            int waitingTimeInMinutes;
+            int waitingTimeInHours;
+            int minutesPerHour = 60;
+
+            Console.Write("Введите количество людей в очереди: ");
+            peoplesInQueue = Convert.ToInt32(Console.ReadLine());
+
+            waitingTime = peoplesInQueue * minutesPerPerson;
+            waitingTimeInMinutes = waitingTime % minutesPerHour;
+            waitingTimeInHours = waitingTime / minutesPerHour;
+
+            Console.WriteLine($"Время ожидания {waitingTimeInHours} час {waitingTimeInMinutes} минут.");
+        }
+    }
+}
